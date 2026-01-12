de76a76f378d5db3822ba7292cafc451d16ae8b7
Cara Steinberg <caralin3@gmail.com>
2026-01-12 11:42:28 -0500

    chore: add react-query and netInfo

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,14 @@
 import '../../global.css';
 
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import NetInfo from '@react-native-community/netinfo';
 import { ThemeProvider } from '@react-navigation/native';
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister';
+import { onlineManager, QueryClient } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
 import { Stack } from 'expo-router';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { KeyboardProvider } from 'react-native-keyboard-controller';
@@ -18,19 +23,54 @@ export default function RootLayout() {
   );
 }
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: Infinity, // Keep data in cache forever
+      // staleTime: Infinity, // Optional: if you never want to refetch data automatically
+    },
+  },
+});
+
+const asyncPersist = createAsyncStoragePersister({
+  storage: AsyncStorage,
+  throttleTime: 3000,
+});
+
 function Providers({ children }: { children: React.ReactNode }) {
   const theme = useThemeConfig();
+
+  useEffect(() => {
+    return NetInfo.addEventListener((state) => {
+      const status =
+        state.isConnected != null &&
+        state.isConnected &&
+        Boolean(state.isInternetReachable);
+      console.log('Network status changed:', status ? 'online' : 'offline');
+      onlineManager.setOnline(status);
+    });
+  }, []);
+
   return (
-    <GestureHandlerRootView
-      style={styles.container}
-      className={theme.dark ? `dark` : undefined}
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister: asyncPersist, maxAge: Infinity }}
+      // onSuccess will be called when the initial restore is finished
+      // resumePausedMutations will trigger any paused mutations
+      // that was initially triggered when the device was offline
+      onSuccess={() => queryClient.resumePausedMutations()}
     >
-      <KeyboardProvider>
-        <ThemeProvider value={theme}>
-          <BottomSheetModalProvider>{children}</BottomSheetModalProvider>
-        </ThemeProvider>
-      </KeyboardProvider>
-    </GestureHandlerRootView>
+      <GestureHandlerRootView
+        style={styles.container}
+        className={theme.dark ? `dark` : undefined}
+      >
+        <KeyboardProvider>
+          <ThemeProvider value={theme}>
+            <BottomSheetModalProvider>{children}</BottomSheetModalProvider>
+          </ThemeProvider>
+        </KeyboardProvider>
+      </GestureHandlerRootView>
+    </PersistQueryClientProvider>
   );
 }
 

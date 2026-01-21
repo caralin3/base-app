ff856e50ed73853d315ed16ae743c74abb187af4
Cara Steinberg <caralin3@gmail.com>
2026-01-21 15:35:54 -0500

    style: remove safe area view excess bottom padding

--- src/components/layout/screen.tsx
+++ src/components/layout/screen.tsx
@@ -1,14 +1,15 @@
-import { SafeAreaView } from '../ui';
+import { type PropsWithChildren } from 'react';
+
+import { SafeAreaView, View } from '../ui';
 import { Header } from './header';
 
-interface ScreenProps {
-  children: React.ReactNode;
+interface ScreenProps extends PropsWithChildren {
   headerProps?: React.ComponentProps<typeof Header>;
 }
 
 export const Screen = ({ children, headerProps }: ScreenProps) => (
-  <SafeAreaView style={{ flex: 1 }}>
+  <SafeAreaView style={{ flex: 1 }} edges={['top']}>
     <Header {...headerProps} />
-    {children}
+    <View style={{ flex: 1 }}>{children}</View>
   </SafeAreaView>
 );
